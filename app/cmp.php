ca9ec758591a9a43a0ae250fdb4659b1af32ae7f
Neeraj1005 <neeraj.tangariya123@gmail.com>
2019-09-29 21:31:43 +0530

    hasMany

--- app/cmp.php
+++ app/cmp.php
@@ -7,5 +7,5 @@ use Illuminate\Database\Eloquent\Model;
 class cmp extends Model
 {
     protected $table='company';
-    public $timestamps = false;
+    
 }
--- app/usr.php
+++ app/usr.php
@@ -8,7 +8,9 @@ class usr extends Model
 {
     public function myCompany()
     {
-    	return $this->hasOne('App\Company');
+    	//return $this->hasOne('App\Company');
+    	
+    	return $this->hasMany('App\Company');
     	//return $this->hasOne('App\cmp','user_id','id');
     }
 }
