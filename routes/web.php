8e41d462f13e84483651894d9d338b4a9f74d7ba
Neeraj1005 <neeraj.tangariya123@gmail.com>
2019-09-29 13:47:44 +0530

    Updated fetch data from model and controller
    in this i have create model controller and
    fetch data from the table

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('db','UserController@index');
+Route::get('list','UserController@index');
+
+//fetch data from table using created model and controller
+Route::get('db','User1Controller@index');
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
